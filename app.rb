ce40e7bc7cdbd1266a3401ea4c55f0fa33d222e3
dynamic-daemon-637b <zl3087@columbia.edu>
2021-06-28 18:41:28 +0000

    app

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+    "My name is Margaret"
+  end
+  get '/hometown' do
+    "My hometown is Shanghai"
+  end
+  get '/favorite-song' do
+    "My favorite song is My heart will go on"
+  end
 end
